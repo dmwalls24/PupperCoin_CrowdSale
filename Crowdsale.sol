c22b65c189bf8f641e621bfb67d07c9591fca05b
dmwalls24 <dmwalls24@gmail.com>
2021-08-13 12:57:29 -0500

    upated puppercoin crowdsale contract

--- Crowdsale.sol
+++ Crowdsale.sol
@@ -8,14 +8,23 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/distribution/RefundablePostDeliveryCrowdsale.sol";
 
 // @TODO: Inherit the crowdsale contracts
-contract PupperCoinSale is {
+contract PupperCoinSale is Crowdsale, MintedCrowdsale, CappedCrowdsale, TimedCrowdsale, RefundablePostDeliveryCrowdsale {
 
     constructor(
-        // @TODO: Fill in the constructor parameters!
-    )
+        uint rate,
+        address payable wallet,
+        PupperCoin token,
+        uint goal,
+        uint open,
+        uint close
+        )
+        
         // @TODO: Pass the constructor parameters to the crowdsale contracts.
-        public
-    {
+        Crowdsale(rate, wallet, token)
+        CappedCrowdsale(goal)
+        TimedCrowdsale(open, close)
+        RefundableCrowdsale(goal)
+        public {
         // constructor can stay empty
     }
 }
@@ -27,12 +36,20 @@ contract PupperCoinSaleDeployer {
 
     constructor(
         // @TODO: Fill in the constructor parameters!
-    )
-        public
-    {
+        string memory name,
+        string memory symbol,
+        address payable wallet,
+        uint goal
+        )
+        public {
         // @TODO: create the PupperCoin and keep its address handy
+        PupperCoin token = new PupperCoin(name, symbol, 0);
+        token_address = address(token);
 
         // @TODO: create the PupperCoinSale and tell it about the token, set the goal, and set the open and close times to now and now + 24 weeks.
+        PupperCoinSale token_sale = new PupperCoinSale(1, wallet, token, goal, now, now + 24 weeks);
+        
+        token_sale_address = address(token_sale);
 
         // make the PupperCoinSale contract a minter, then have the PupperCoinSaleDeployer renounce its minter role
         token.addMinter(token_sale_address);
